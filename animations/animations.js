0513aecda563ccde4d5a26cbdb192924df912638
Robert Dudley <robert.dudley@dillards.com>
2016-07-29 14:55:10 -0700

    animations notes

--- animations/animations.js
+++ animations/animations.js
@@ -1,4 +1,32 @@
-// Animations 
+// Animations Panel
 // Guillermo Rauch: @rauchg, socket.io
 // Swizec: @swizec
+// Estelle Wyle
+
+// reduce the amount of elements your are animating
+	// if it's not necesary, remove. or only show when necessary
+
+// transform is on GPU
+// other animations are not on GPU
+
+//servo?? animations
+
+//http2 guidlines can make it easier in future
+
+// animation should be progressive enhancement
+
+// http://speedperception.meteorapp.com/
+
+// Animation API is JS, but not widely supported
+
+// Getting started with Animations
+	// TDD when it comes to animation
+	// Making It Snow talk
+	// CodePen
+
+	// ReactMotion
+	// ReactTransitionGroup
+
+	// http://framerjs.com/
+	// http://greensock.com/
 
