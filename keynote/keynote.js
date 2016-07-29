ce3030d33c75ca21f38f4d790d97bdfe1ce275bd
Robert Dudley <robert.dudley@dillards.com>
2016-07-29 10:05:54 -0700

    Jenna Zeigen Keynote notes

---
+++ keynote/keynote.js
@@ -0,0 +1,37 @@
+// ForwardJS Keynote
+
+// code is nothing if you don't pay attention to the people behind it
+
+// How Your Brain is Conspiring Against You Making Good Software
+// Jenna Zeigen, @zeigenvector
+	// Digital Ocean, EmpireJS
+	// Cognitive Science: Thesis on how brain processes puns, "What's so funny?"
+
+// Dan Airely "Humans are predicably irrational"
+
+// On Being Logical
+	// All P are M.
+	// All S are M.
+	// Therefore, all S are P.
+
+	// A 6 J 7
+
+// Bugs and debugging
+	// Debugging is twice as hard as writing the code in the first place
+	// interpret and favor info in a way that matches to our beliefs
+
+	// even distrust evidence that goes against our prior belief
+		// i don't want my code to be wrong
+
+	// We tend to be rigid in how we approach a problem
+	// we tend block problem solutions based on past experiences
+
+	// Eureka, i solved it!
+	// no idea we were going to solve problem, even 30 seconds priot to completion
+
+	// Breaks are more important than you think
+	
+// Sprint planning
+
+
+// Other's Code
